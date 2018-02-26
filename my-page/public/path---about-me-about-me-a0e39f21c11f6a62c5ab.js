b94903d9f5023676cc8ce4f18755ed380d760157
JWood <1987boxior@gmail.com>
2018-02-27 00:52:25 +0200

    add public folder

---
+++ my-page/public/path---about-me-about-me-a0e39f21c11f6a62c5ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xbd67ee86d8f6],{383:function(e,t){e.exports={pathContext:{}}}});
+//# sourceMappingURL=path---about-me-about-me-a0e39f21c11f6a62c5ab.js.map
---
+++ my-page/public/path---portfolio-portfolio-a0e39f21c11f6a62c5ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x74f1470318e8],{385:function(t,e){t.exports={pathContext:{}}}});
+//# sourceMappingURL=path---portfolio-portfolio-a0e39f21c11f6a62c5ab.js.map
